52595b05bc3cf581d4ee5f880b838f1aa205c795
Don MF Lee <lee@algonquinstudios.com>
2014-10-22 16:06:18 -0400

    initiang author to DMFL

---
+++ Server.js
@@ -0,0 +1,6 @@
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Dis BOGIS node server in da cloud so sweet you be cryin. Yea... you thought it was rain. \n');
+}).listen(port);
