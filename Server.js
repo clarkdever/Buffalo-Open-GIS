4a59842d8ace2eba6815b64a35b8563b7b259262
Don MF Lee <lee@algonquinstudios.com>
2014-10-22 16:14:56 -0400

    changing to hello azure

--- Server.js
+++ Server.js
@@ -2,5 +2,5 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Dis BOGIS node server in da cloud so sweet you be cryin. Yea... you thought it was rain. \n');
+  res.end('Dis BOGIS node server in da cloud so sweet you be cryin. Yea... you thought it was rain. -DMFL\n');
 }).listen(port);
