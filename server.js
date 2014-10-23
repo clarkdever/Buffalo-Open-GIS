f779a3c15d54c40a7e36c1d6c5c8f3cb52ac12bc
unknown <Raines@RAINES-WIN8.development.algonquinstudios.com>
2014-10-22 21:24:09 -0400

    sbr commit

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
 var http = require('http');
-
-var finalhandler = require('finalhandler');
-var serveStatic = require('serve-static');
-
-var serve = serveStatic("./");
-
-var server = http.createServer(function(req, res){
-  var done = finalhandler(req, res)
-  serve(req, res, done)
-});
-
-server.listen(80);
+var port = process.env.port || 1337;
+http.createServer(function(req, res){
+res.writeHead(200,{ 'Content-Type':'text/plain'});
+	res.end('Hello World\n');
+}).listen(port);
