7197db8eeaa853869ef30d6d969b31abdd3bbbfc
Clark Dever <clark.dever@algonquinstudios.com>
2014-10-23 09:40:56 -0400

    Integrated Nick's design and super simple node server that will only load index.html

--- Server.js
+++ Server.js
@@ -1,6 +1,16 @@
-var http = require('http')
+var http = require('http'),
+path = require('path'),
+fs = require('fs');
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('I need to SSH in and install stuff with npm -CMFD\n');
+  fs.readFile("./index.html",function(err,contents){
+      if(!err){
+          //if there was no error
+          //send the contents with the default 200/ok header
+          res.end(contents);
+      } else {
+          //for our own troubleshooting
+          console.dir(err);
+      };
+  });
 }).listen(port);
