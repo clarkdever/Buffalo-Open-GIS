eb4fc38ec6e594e11cd7be8231b72baa1ce34c07
unknown <Raines@RAINES-WIN8.development.algonquinstudios.com>
2014-10-22 21:30:54 -0400

    sbr commit

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var http = require('http');
 var port = process.env.port || 1337;
 http.createServer(function(req, res){
-res.writeHead(200,{ 'Content-Type':'text/plain'});
-	res.end('Hello World\n');
+	res.status(200).sendFile('/index.html');
 }).listen(port);
