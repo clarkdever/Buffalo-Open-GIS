e7adb1c54ddcb0489361b0a5ca6c039e40ac75d1
Clark Dever <clark.dever@algonquinstudios.com>
2014-10-24 18:19:00 -0400

    Added AngularFire code into project, removed Yeoman generated app directory

--- assets/js/ng/app.js
+++ assets/js/ng/app.js
@@ -1,21 +1,21 @@
-/*
-Code From: http://viralpatel.net/blogs/angularjs-routing-and-views-tutorial-with-example/
-*/
+'use strict';
 
-var sampleApp = angular.module('phonecatApp', []);
-
-sampleApp .config(['$routeProvider',
-  function($routeProvider) {
-    $routeProvider.
-      when('/addOrder', {
-        templateUrl: 'templates/add-order.html',
-        controller: 'AddOrderController'
-      }).
-      when('/showOrders', {
-        templateUrl: 'templates/show-orders.html',
-        controller: 'ShowOrdersController'
-      }).
-      otherwise({
-        redirectTo: '/addOrder'
-      });
-  }]);
+/**
+ * @ngdoc overview
+ * @name bogisApp
+ * @description
+ * # bogisApp
+ *
+ * Main module of the application.
+ */
+angular.module('bogisApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase',
+    'firebase.utils',
+    'simpleLogin'
+  ]);
