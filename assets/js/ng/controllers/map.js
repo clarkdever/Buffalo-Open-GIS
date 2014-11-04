a3dd295e72ece7018c6cbb7910bc892014fce3a8
BuffaloOpenGIS\AlgonquinAzureDeploy <nicholasgianadda@sk-laptop.development.algonquinstudios.com>
2014-11-04 14:31:52 -0500

    Adding Map Controller

---
+++ assets/js/ng/controllers/map.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bogisApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bogisApp
+ */
+angular.module('bogisApp')
+  .controller('MapCtrl', function ($scope) {
+  	console.log("MapCtrl");
+  });
